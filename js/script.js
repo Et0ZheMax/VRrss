c739dc5b6e3d4cf89cf5437d236451b5771bfb83
Et0ZheMax <numbxxl@yandex.ru>
2025-03-03 23:36:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,400 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  /*try {
+    IMask(document.getElementById('phone-mask'), {
+      mask: '/^123'
+    });
+  } catch (e) {
+    console.log(e);
+  }*/
+  
+  try {
+    var elems = document.querySelectorAll('.select-account');
+    elems.forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        window.location.replace('/accounts?current=' + encodeURIComponent(el.getAttribute("data-value")))
+      })})
+  } catch (e) {
+    console.log(e)
+  }
+
+  /*try {
+    var elems = document.querySelectorAll('.select-session');
+    elems.forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        window.location.replace(el.getAttribute("data-value"))
+      })})
+  } catch (e) {
+    console.log(e)
+  }*/
+
+  try {
+    var elems = document.querySelectorAll('.select-emulator');
+    elems.forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', "/pc/emulator?emulator=" + el.getAttribute("data-value"), false);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+              location.reload();
+            } else {
+              console.log(xhr.responseText)
+            }
+        }
+
+        xhr.send();
+      })})
+  } catch (e) {
+    console.log(e)
+  }
+
+  try {
+    function customSelect(elSelectNative, elSelectCustom) {
+      // var elSelectNative = document.querySelector(".js-selectNative");
+      // var elSelectCustom = document.querySelector(".js-selectCustom");
+      var elSelectCustomBox = elSelectCustom.children[0];
+      var elSelectCustomOpts = elSelectCustom.children[1];
+      var customOptsList = Array.from(elSelectCustomOpts.children);
+      var optionsCount = customOptsList.length;
+      var defaultLabel = elSelectCustomBox.getAttribute("data-value");
+      var optionChecked = "";
+      var optionHoveredIndex = -1;
+      
+    
+      // Toggle custom select visibility when clicking the box
+      elSelectCustomBox.addEventListener("click", function (e) {
+        var disabled = elSelectCustom.classList.contains("disabled");
+        var isClosed = !elSelectCustom.classList.contains("isActive");
+        if (!disabled){
+          if (isClosed) {
+            openSelectCustom();
+          } else {
+            closeSelectCustom();
+          }
+      }
+      });
+      function openSelectCustom() {
+        elSelectCustom.classList.add("isActive");
+        if (optionChecked) {
+          var optionCheckedIndex = customOptsList.findIndex(function (el) {
+            return el.getAttribute("data-value") === optionChecked;
+          });
+          updateCustomSelectHovered(optionCheckedIndex);
+        }
+    
+        // Add related event listeners
+        document.addEventListener("click", watchClickOutside);
+        document.addEventListener("keydown", supportKeyboardNavigation);
+      }
+      function closeSelectCustom() {
+        elSelectCustom.classList.remove("isActive");
+        // elSelectCustom.setAttribute("aria-hidden", true);
+        updateCustomSelectHovered(-1);
+    
+        // Remove related event listeners
+        document.removeEventListener("click", watchClickOutside);
+        document.removeEventListener("keydown", supportKeyboardNavigation);
+      }
+      function updateCustomSelectHovered(newIndex) {
+        var prevOption = elSelectCustomOpts.children[optionHoveredIndex];
+        var option = elSelectCustomOpts.children[newIndex];
+        if (prevOption) {
+          prevOption.classList.remove("isHover");
+        }
+        if (option) {
+          option.classList.add("isHover");
+        }
+        optionHoveredIndex = newIndex;
+      }
+      function updateCustomSelectChecked(value, text) {
+        var prevValue = optionChecked;
+        var elPrevOption = elSelectCustomOpts.querySelector("[data-value=\"".concat(prevValue, "\""));
+        var elOption = elSelectCustomOpts.querySelector("[data-value=\"".concat(value, "\""));
+        if (elPrevOption) {
+          elPrevOption.classList.remove("isActive");
+        }
+        if (elOption) {
+          elOption.classList.add("isActive");
+        }
+        elSelectCustomBox.textContent = text;
+        optionChecked = value;
+      }
+      function watchClickOutside(e) {
+        var didClickedOutside = !elSelectCustom.contains(e.target);
+        if (didClickedOutside) {
+          closeSelectCustom();
+        }
+      }
+      function supportKeyboardNavigation(e) {
+        // press down -> go next
+        if (e.keyCode === 40 && optionHoveredIndex < optionsCount - 1) {
+          var index = optionHoveredIndex;
+          e.preventDefault(); // prevent page scrolling
+          updateCustomSelectHovered(optionHoveredIndex + 1);
+        }
+    
+        // press up -> go previous
+        if (e.keyCode === 38 && optionHoveredIndex > 0) {
+          e.preventDefault(); // prevent page scrolling
+          updateCustomSelectHovered(optionHoveredIndex - 1);
+        }
+    
+        // press Enter or space -> select the option
+        if (e.keyCode === 13 || e.keyCode === 32) {
+          e.preventDefault();
+          var option = elSelectCustomOpts.children[optionHoveredIndex];
+          var value = option && option.getAttribute("data-value");
+          if (value) {
+            elSelectNative.value = value;
+            updateCustomSelectChecked(value, option.textContent);
+          }
+          closeSelectCustom();
+        }
+    
+        // press ESC -> close selectCustom
+        if (e.keyCode === 27) {
+          closeSelectCustom();
+        }
+      }
+    
+      // Update selectCustom value when selectNative is changed.
+      elSelectNative.addEventListener("change", function (e) {
+        var value = e.target.value;
+        var elRespectiveCustomOption = elSelectCustomOpts.querySelectorAll("[data-value=\"".concat(value, "\"]"))[0];
+        updateCustomSelectChecked(value, elRespectiveCustomOption.textContent);
+      });
+    
+      // Update selectCustom value when an option is clicked or hovered
+      customOptsList.forEach(function (elOption, index) {
+        elOption.addEventListener("click", function (e) {
+          var value = e.target.getAttribute("data-value");
+    
+          // Sync native select to have the same value
+          elSelectNative.value = value;
+          updateCustomSelectChecked(value, e.target.textContent);
+          closeSelectCustom();
+        });
+        elOption.addEventListener("mouseenter", function (e) {
+          updateCustomSelectHovered(index);
+        });
+    
+        // TODO: Toggle these event listeners based on selectCustom visibility
+      });
+    }
+
+    document.querySelectorAll(".select").forEach(function(el) {
+      const native = el.querySelector('.js-selectNative');
+      const custom = el.querySelector('.js-selectCustom')
+      customSelect(native, custom);
+    })
+  } catch(e) {
+    console.log(e);
+  }
+
+  try {
+    const btns = document.querySelectorAll('.accounts__row__btn');
+    btns.forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        const parent = el.closest('.accounts__row');
+        const block = parent.querySelector('.accounts__row__body');
+        block.classList.toggle("active");
+      });
+    })
+
+  } catch(e) {
+    console.log(e);
+  }
+
+  try {
+    const burgerBtn = document.querySelector('.mob-menu__burger');
+    const closeBtn = document.querySelector('.mob-menu-plate__close');
+    const menu = document.querySelector('.mob-menu-plate');
+    const overlay = document.querySelector('.overlay');
+
+    burgerBtn.addEventListener("click", function(e) {
+      menu.classList.add('active');
+      overlay.classList.add('active');
+    });
+
+    closeBtn.addEventListener("click", function(e) {
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+
+  } catch(e) {
+    console.log(e);
+  }
+
+  try {
+    const saveBtn = document.getElementById("accounts_save_btn")
+    const currentAcc = document.getElementById("currentAccount")
+
+    saveBtn.addEventListener("click", function(e){
+      var elems = document.querySelectorAll('.fbots_settings')
+      var intsettings = new Object()
+      var stringsettings = new Object()
+
+      for (var i = 0; i < elems.length; i++) {
+        if (elems[i].type == 'checkbox') {
+          intsettings[elems[i].name] = Number(elems[i].checked)
+        } else if (elems[i].type == 'select-one') {
+          intsettings[elems[i].name] = Number(elems[i].value)
+        } else if (elems[i].type == 'number') {
+          intsettings[elems[i].name] = Number(elems[i].value)
+        } else {
+          stringsettings[elems[i].name] = elems[i].value;
+        }
+      }
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', "/settings", true);
+      xhr.onload = function() {
+        if (xhr.status != 200) {
+          console.log(xhr.responseText)
+        }
+        location.reload();
+      }
+
+      var data = new Object();
+      data["name"] = currentAcc.innerHTML
+      data["int-settings"] = intsettings
+      data["string-settings"] = stringsettings
+
+      xhr.send(JSON.stringify(data))
+
+//      console.log(g)
+    });
+  } catch (e) {
+    console.log(e)
+  }
+/*
+  try {
+    const removeBtns = document.querySelectorAll('.pc__accounts__body__row__remove');
+    const removePopup = document.getElementById("remove-account");
+    const overlay = document.querySelector('.overlay');
+    removeBtns.forEach(function(elem) {
+      elem.addEventListener("click", function(e) {
+        removePopup.classList.add('active');
+        overlay.classList.add("active");
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+*/
+  try{
+    const removeAccountBtn = document.getElementById("remove_account_btn");
+    const removePopup = document.getElementById("remove-account");
+    const overlay = document.querySelector('.overlay');
+    
+    removeAccountBtn.addEventListener("click", function(e){
+      removePopup.classList.add('active');
+      overlay.classList.add("active");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try{
+    const removeAccountBtn = document.getElementById("remove_session_btn");
+    const removePopup = document.getElementById("remove-session");
+    const overlay = document.querySelector('.overlay');
+    
+    removeAccountBtn.addEventListener("click", function(e){
+      removePopup.classList.add('active');
+      overlay.classList.add("active");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    const closePopupBtn = document.querySelectorAll(".close-popup");
+    const overlay = document.querySelector('.overlay');
+
+    closePopupBtn.forEach(function(btn) {
+      btn.addEventListener("click", function(e) {
+        btn.closest(".popup-wrapper").classList.remove("active");
+        overlay.classList.remove("active");
+      });
+    })
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    const removePopup = document.getElementById("add-session");
+    const overlay = document.querySelector('.overlay');
+    const addSessionBtn = document.querySelectorAll('.add-session');
+
+    addSessionBtn.forEach(function(elem) {
+      elem.addEventListener("click", function(e) {
+        removePopup.classList.add('active');
+        overlay.classList.add("active");
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    const popup = document.getElementById("create-account");
+    const overlay = document.querySelector('.overlay');
+    const btn = document.querySelectorAll('.open-create-popup');
+
+    btn.forEach(function(elem) {
+      elem.addEventListener("click", function(e) {
+        popup.classList.add('active');
+        overlay.classList.add("active");
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try{
+    const timeoutPopup = document.getElementById("timeout");
+    const overlay = document.querySelector('.overlay');
+    const btn = document.querySelectorAll('.open-timeout-popup');
+
+    btn.forEach(function(elem) {
+      elem.addEventListener("click", function(e) {
+        timeoutPopup.classList.add('active');
+        overlay.classList.add("active");
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    const btn = document.querySelector(".add-account-session");
+    const normal_mode = document.getElementById("pc_session_area_normal");
+    const edit_mode = document.getElementById("pc_session_area_edit");
+    const normalBtn = document.querySelector(".pc__accounts__body__normal-mode");
+
+    btn.addEventListener("click", function(e) {
+      normal_mode.hidden = true
+      edit_mode.hidden = false
+    });
+
+    normalBtn.addEventListener("click", function(e) {
+      normal_mode.hidden = false
+      edit_mode.hidden = true
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+
+/*$(window).keyup(function(e){
+	var target = $('.checkbox-ios input:focus');
+	if (e.keyCode == 9 && $(target).length){
+		$(target).parent().addClass('focused');
+	}
+});
+ 
+$('.checkbox-ios input').focusout(function(){
+	$(this).parent().removeClass('focused');
+});*/
